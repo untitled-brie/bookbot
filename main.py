46b8a64b7d3e0472df70c27c18cd1e0f0027283e
untitled-brie <untitled.project17@gmail.com>
2025-06-01 10:13:10 +0700

    4: character dict to list v.1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, character_count
+from stats import word_count, character_count,sorted_chara_count
 
 def get_book_content(relative_path):
     with open(relative_path) as book:
@@ -9,6 +9,10 @@ def main():
     total_words = word_count(get_book_content("books/frankenstein.txt"))
     print(f"{total_words} words found in the document")
     each_chara_count_dict = character_count(get_book_content("books/frankenstein.txt"))
-    print(each_chara_count_dict)
+    # print(each_chara_count_dict)
+
+    # test
+    sorted_chara_count(each_chara_count_dict)
+    
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,12 @@ def character_count(text):
         else:
             count_dict[chara] += 1
     return count_dict
+
+def sorted_chara_count(unsorted_dict):
+    new_list = []
+    for key,value in unsorted_dict.items():
+        new_dict = {}
+        new_dict["char"] = key
+        new_dict["num"] = value
+        new_list.append(new_dict)
+    print(new_list)
